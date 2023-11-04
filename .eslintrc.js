116fc951d5f1d8498bf19a889a98c6904c8d0e62
Neto test <strokova-92@mail.ru>
2023-11-04 13:38:53 +0300

    Update commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: 'standard',
+  extends: 'plugin:prettier/recommended',
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
-    'no-debugger': 'off'
-  }
-}
+    'no-debugger': 'off',
+  },
+};
